34e88c9fda0014fdd7a50265bbfeeed3c47d9d6c
Yu Nakata <y-nakata@outlook.com>
2020-03-01 03:13:03 +0000

    upload archives to S3 with layers.sh

--- layers.sh
+++ layers.sh
@@ -12,4 +12,12 @@ mkdir dist-matplotlib
 pip install -t ./dist-matplotlib matplotlib
 zip -r matplotlib.zip ./dist-matplotlib/*
 
+echo "BUCKET_NAME is ${BUCKET_NAME}"
+if [ -n "${BUCKET_NAME}" ]; then
+  echo "Uploading archives to S3..."
+  aws s3 cp requests.zip s3://${BUCKET_NAME}/requests.zip
+  aws s3 cp pandas.zip s3://${BUCKET_NAME}/pandas.zip
+  aws s3 cp matplotlib.zip s3://${BUCKET_NAME}/matplotlib.zip
+fi
+
 echo "done"  
